d384db3e4bad5873377ea6fd3690770085c7d9a5
CE-sw <2seowon@naver.com>
2022-08-09 21:25:41 +0900

    login issue

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ users = {
 
 @app.route('/')
 def index():
-    # html file은 templates 폴더에 위치해야 함
-    return render_template('index.html', username=session.get("id"))
+    login = False
+    try: 
+        if session['id']: login = True
+    except : pass
+
+    return render_template('index.html', username=session.get("id"), login=login)
 
 @app.route('/login')
 def login():
@@ -41,37 +45,57 @@ def logout():
 
 @app.route('/question')
 def question():
+    login = False
+    try: 
+        if session['id']: login = True
+    except : pass
     db = pymysql.connect(host='us-cdbr-east-06.cleardb.net', port=3306, user='bbc263342cae56', passwd='33c946ea', db='heroku_0a4b1cb2682c753', charset='utf8')
     tag = db.cursor()
     sql = "select * from tag"
     tag.execute(sql)
     result = tag.fetchall()
     db.close()
-    return render_template('question.html', tag=result, username=session.get("id"))
+    return render_template('question.html', tag=result, username=session.get("id"), login=login)
 
 @app.route('/answer')
 def answer():
+    login = False
+    try: 
+        if session['id']: login = True
+    except : pass
     db = pymysql.connect(host='us-cdbr-east-06.cleardb.net', port=3306, user='bbc263342cae56', passwd='33c946ea', db='heroku_0a4b1cb2682c753', charset='utf8')
     article = db.cursor()
     article.execute("select * from articles")
     articles = article.fetchall()
     articles = list(articles)
     db.close()
-    return render_template('answer.html', article=articles, username=session.get("id"))
+    return render_template('answer.html', article=articles, username=session.get("id"), login=login)
 
 @app.route('/user')
 def user():
-    return render_template('user.html', username=session.get("id"))
+    login = False
+    try: 
+        if session['id']: login = True
+    except : pass
+    return render_template('user.html', username=session.get("id"), login=login)
 
 @app.route('/write')
 def write():
-    return render_template('write.html', username=session.get("id"))
+    login = False
+    try: 
+        if session['id']: login = True
+    except : pass
+    return render_template('write.html', username=session.get("id"), login=login)
 
 @app.route('/post', methods = ["get", "post"])
 def post():
+    login = False
+    try: 
+        if session['id']: login = True
+    except : return render_template('errorwrite.html')
+
     id = session.get("id")
-    if id == None:
-        return render_template('errorwrite.html')
+
     tag = str(request.args.get('tag'))
     title = str(request.args.get('title'))
     content = str(request.args.get('content'))
@@ -79,10 +103,14 @@ def post():
     article = db.cursor()
     article.execute("INSERT INTO articles VALUES(%s, %s, %s, %s, %s)", [None, id, title, content, tag])
     db.commit();
-    return redirect(url_for('answer'))
+    return redirect(url_for('answer'), login=login)
     
 @app.route('/detail/<int:post>')
 def detail(post):
+    login = False
+    try: 
+        if session['id']: login = True
+    except : pass
     idx = int(post)
     db = pymysql.connect(host='us-cdbr-east-06.cleardb.net', port=3306, user='bbc263342cae56', passwd='33c946ea', db='heroku_0a4b1cb2682c753', charset='utf8')
     article = db.cursor()
@@ -95,7 +123,7 @@ def detail(post):
     title = result[0][2]
     content = result[0][3]
     db.close()
-    return render_template('detail.html', id=id, tag=tag, title=title, content=content, username=session.get("id"))
+    return render_template('detail.html', id=id, tag=tag, title=title, content=content, username=session.get("id"), login=login)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8080")
